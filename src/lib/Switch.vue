5257f3f84b79b8d852daa6726d744b53a4fda19b
Apple <apple@192.168.10.101>
2020-11-09 07:39:45 +0800

    add

---
+++ src/lib/Switch.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button><span>
+  我是switch
+</span></button>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
